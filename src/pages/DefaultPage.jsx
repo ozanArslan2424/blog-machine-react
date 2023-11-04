76875727dffc8b8fff8aa5a81d5e8d03b9f2b552
ozanArslan2424 <136094471+ozanArslan2424@users.noreply.github.com>
2023-11-05 00:50:13 +0300

    assets arranged?

--- src/pages/DefaultPage.jsx
+++ src/pages/DefaultPage.jsx
@@ -7,11 +7,11 @@ import DefBtnBoard from "../sections/DefBtnBoard";
 export default function DefaultPage() {
     return (
         <>
-        <Header headerImage={HeaderImg1}/>
-        <div className="flex responsive-flex justify-center gap-4 px-4 w-screen">
-            <DefBtnBoard />
-            <Previews />
-        </div>
+            <Header headerImage={HeaderImg1} />
+            <div className="flex responsive-flex justify-center gap-4 px-4 w-screen">
+                <DefBtnBoard />
+                <Previews />
+            </div>
         </>
     )
 }
