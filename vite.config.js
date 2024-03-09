b1b36ae8da587c16f8cfcd2a4257a1f91ac8d087
ozanArslan2424 <136094471+ozanArslan2424@users.noreply.github.com>
2024-03-09 20:53:52 +0300

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/blogmachine/',
+  base: '/blogmachine-v2/',
   plugins: [react()],
   // other options...
 })
